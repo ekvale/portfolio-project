ee907d44ab03911a3970d4bb6b053effbabe1392
ekvale <ekvale@gmail.com>
2018-08-25 10:33:29 +0000

    tweks

---
+++ portfolio/local_settings.py
@@ -0,0 +1,19 @@
+
+
+SECRET_KEY = '(=+fpf!)&(nnr#@i)fisy%z$$balloonv64i5fdsd$holidayrs66b45%(l7+*waertl'
+
+
+ALLOWED_HOSTS = ['142.93.195.241']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'djangodbman',
+        'PASSWORD' : 'django1234',
+        'HOST': 'localhost',
+        'PORT' : '5432'
+    }
+}
+
+DEBUG = False
